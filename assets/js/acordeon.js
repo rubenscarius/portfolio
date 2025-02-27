de2d9d9ff2d404dd967a46b2f5df8671e28eddb5
rubenscarius <rubenscarius@msn.com>
2025-02-26 21:20:53 -0300

    add icon arrow rotate

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -3,5 +3,7 @@ const acordeon = document.querySelectorAll('.acordeon');
 acordeon.forEach(item => {
     item.addEventListener('click', () => {
         item.classList.toggle('open');
+        const icon = item.querySelector('#icon-arrow');
+        icon.classList.toggle('rotate');
     });
 });
